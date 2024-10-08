098ae294c188a14d945663f7bf31fd690e7790fd
yayorome <romero.yair@gmail.com>
2024-10-07 19:45:39 -0600

    Github example

---
+++ p3/github.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {
+    token = "ACCESS_TOKEN"
+}
+
+resource "github_repository" "testrepo" {
+  name = "example"
+  description = "terraform example"
+
+  visibility = "public"
+}
